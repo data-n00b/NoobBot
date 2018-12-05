ae59ca9ea1e43135300e1db6dca8b2fafb88f1af
Arvind <sriram4rajipr@gmail.com>
2018-12-05 00:10:30 -0500

    Keys

--- src/NoobBot.py
+++ src/NoobBot.py
@@ -237,10 +237,6 @@ if __name__ == '__main__':
     access_token = '<Your Access token>'; 
     access_token_secret = '<Your Access Token Secret>';
     '''
-    consumer_key = 'x45FNED54ZkOzcWpK5I7KNkmT'; 
-    consumer_secret = '9N4ABRaa9m6F2efgqlO1VP6014yoFcR1y29V51PuSMrOpwPpsX'; 
-    access_token = '1057119039569489922-ltle8eR6UMBaYM0xwOYJ9GHPNU7PDF'; 
-    access_token_secret = 'WIGeuVyguIYwjeA5lXJJWPUePK8KTIxEKuM5jPFT3DBcs';
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     
